dbfe402100f47eac7abdfa5ecc37e134d69b9834
Klim <klimberegnoy098@gmail.com>
2024-12-01 13:37:11 +0300

    hw2.6 - Embedded objects

--- gameJS.js
+++ gameJS.js
@@ -32,10 +32,8 @@ function arefmetics() {
         const num2 = Math.floor(Math.random() * 10) + 1;
         const operator = operators[Math.floor(Math.random() * operators.length)];
 
-
         let task = `${num1} ${operator} ${num2}`;
         let correctAnswer;
-
 
         switch (operator) {
             case '+':
@@ -55,18 +53,70 @@ function arefmetics() {
         return { task, correctAnswer };
     }
 
-
     const { task, correctAnswer } = generateRandomTask();
-
 
     let userAnswer = prompt(`Решите задачу: ${task}`);
 
     userAnswer = parseFloat(userAnswer);
-
 
     if (userAnswer === correctAnswer) {
         alert('Верно!');
     } else {
         alert(`Неверно! Правильный ответ: ${correctAnswer}`);
     }
+}
+
+
+function text() {
+    function reverseText() {
+        const userInput = prompt("Введите текст:"); 
+        
+        if (userInput !== null) {
+            const reversed = userInput.split('').reverse().join('');
+    
+            alert("Перевернутый текст: " + reversed);
+        } else {
+            alert("Текст не был введен.");
+        }
+    }
+    
+    reverseText();
+}
+
+function quiz() {
+    // Массив вопросов и правильных ответов
+const quiz = [
+    {
+        question: "Какой цвет небо?",
+        options: ["1. Красный", "2. Синий", "3. Зеленый"],
+        correctAnswer: 2 
+    },
+    {
+        question: "Сколько дней в неделе?",
+        options: ["1. Шесть", "2. Семь", "3. Восемь"],
+        correctAnswer: 2
+    },
+    {
+        question: "Сколько у человека пальцев на одной руке?",
+        options: ["1. Четыре", "2. Пять", "3. Шесть"],
+        correctAnswer: 2
+    }
+];
+
+// Функция для проведения викторины
+function runQuiz() {
+    let score = 0; 
+
+    for (let i = 0; i < quiz.length; i++) {
+        const question = quiz[i];
+        let userAnswer = prompt(`${question.question}\n${question.options.join('\n')}`);
+        
+        if (parseInt(userAnswer) === question.correctAnswer) {
+            score++;
+        }
+    }
+    alert(`Вы ответили правильно на ${score} из ${quiz.length} вопросов.`);
+}
+
+runQuiz();
 }
